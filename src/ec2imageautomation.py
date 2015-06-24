c79bf7f6720004a1f742fab145c8a9a4da6cde88
TvHeadedRobots <pneusolematic@mac.com>
2015-06-24 15:02:34 -0400

    Added error checking, logging & wrapped some code
    Added error checking for fore AMI creation
    Added logging
    Wrapped code to create image in function

--- src/ec2imageautomation.py
+++ src/ec2imageautomation.py
@@ -14,7 +14,11 @@ import subprocess
 # setup EC2 API Tools commands
 ec2DescTagsCmd = 'c:/ec2/ec2-api-tools-1.7.4.0/bin/ec2-describe-tags.cmd'
 # c:/ec2/ec2-api-tools-1.7.4.0/bin/ec2-create-image.cmd [instanceId] -n "[image name]" --no-reboot
-ec2ImgCmd = 'c:/ec2/ec2-api-tools-1.7.4.0/bin/ec2-create-image.cmd'
+ec2StatusCmd = 'ec2-describe-instance-status'
+curdate = time.strftime('%Y%m%d-%H%M')
+
+# setup log file
+amiLog = open('amiLog.txt', 'a')
 
 # check for backup value arg. If no argument assume "1"
 # should add input prompt
@@ -51,7 +55,6 @@ def get_backup_instance(backupVal):
 # iterate through ec2ImgInstanceId
 def get_backup_img_name(ec2ImgInstanceId):
     ec2ImgName = []
-    curdate = time.strftime("%Y%m%d")
 
     # iterate through tag[] to get "Name" name / value for instances in ec2ImgInstanceId
     for instanceId in ec2ImgInstanceId:
@@ -69,25 +72,82 @@ def get_backup_img_name(ec2ImgInstanceId):
     # return list of constructed images names for backup            
     return ec2ImgName
 
+def create_ec2_img(backupInstanceList, backupImgNameList):
+    # check to make sure that backupInstanceList list is same size as backupImgName list
+    
+    ec2ImgCmd = 'c:/ec2/ec2-api-tools-1.7.4.0/bin/ec2-create-image.cmd'
+    dupeAmiNames = []
+    
+    if len(backupInstanceList) != len(backupImgNameList):
+        print(len(backupImgNameList)) 
+        print("ERROR: List of instances to backup is not the same size as backup image name list.\r\nSomething is wrong and I'm not going any further.")
+        print('len(backupImgNameList) = ')
+        print(len(backupImgNameList))
+        print('len(backupInstanceList) = ')
+        print(len(backupInstanceList))
+        return 
+    else:
+        for n in range(len(backupInstanceList)):
+            if (len(sys.argv) > '1' and sys.argv[2] == 'reboot'):
+                ec2ImgCmd = ec2ImgCmd + ' ' + backupInstanceList[n] + ' -n "' + backupImgNameList[n] + '"'
+                print('Preforming backup of ' + backupInstanceList[n] + ' with reboot')
+            else:
+                ec2ImgCmd = ec2ImgCmd + ' ' + backupInstanceList[n] + ' -n "' + backupImgNameList[n] + '"' + ' --no-reboot'
+                print('Preforming backup of ' + backupInstanceList[n] + ' with no reboot')
+                
+            exeEc2ImgCmd = subprocess.Popen(ec2ImgCmd,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+
+        print('Creating AMI image named: ' + backupImgNameList[n] + '...')
+        
+        retVal = []
+        # get results from command 
+        ec2ImgStdOut = exeEc2ImgCmd.stdout.readline()
+        ec2ImgErrOut = exeEc2ImgCmd.stderr.readline()
+        
+        # parse stdout command result
+        retVal = ec2ImgStdOut.split('\t')
+                
+        #check stdout result to see if an image was created
+        if len(retVal) > 0 and retVal[0] == 'IMAGE':
+            amiId = retVal[1].replace('\r\n','')
+            amiLog.write('AMI created:' + amiId + '\t' + backupImgNameList[n] + '\t' + curdate + '\r\n')
+            amiLog.close()
+            print('Image ' + amiId + ' created')
+            
+        # check to see if there was an error returned
+        if ec2ImgErrOut != '':
+            #print('error: ' + ec2ImgErrOut)
+            errRetVal = []
+            errRetVal = ec2ImgErrOut.split(' ')
+
+            # check to see if image name already exists
+            if errRetVal[0] == 'Client.InvalidAMIName.Duplicate:':
+                amiLog.write(backupImgNameList[n] + ' already exists\t' + curdate + '\t' + errRetVal[0] + '\r\n')
+                amiLog.close()
+                # add duplicate AMI names to list to rerun
+                dupeAmiNames.append(backupImgNameList[n])
+                print('No AMI created')
+                print(ec2ImgErrOut)
+            else:
+                print('Something happened...')
+                print('stdout: ' + ec2ImgStdOut)
+                print('errout: ' + ec2ImgErrOut)
+
+
+        return
+
+
 # call functions to get backup instances and image names
 backupInstances = get_backup_instance(backupValArg)
-backupImgName = get_backup_img_name(backupInstances)
+backupImgNames = get_backup_img_name(backupInstances)
+# call function to create images
+ec2ImgRun = create_ec2_img(backupInstances, backupImgNames)
 
-# check to make sure that backupInstances list is same size as backupImgName list
-if len(backupInstances) != len(backupImgName):
-    print("ERROR: List of instances to backup is not the same size as backup image name list.\r\nSomething is wrong and I'm not going any further.")
-    print('len(backupImgName) = ')
-    print(len(backupImgName))
-    print('len(backupInstances) = ')
-    print(len(backupInstances))
-else:
-    for n in range(len(backupInstances)):
-        if (len(sys.argv) > '1' and sys.argv[2] == 'reboot'):
-            ec2ImgCmd = ec2ImgCmd + ' ' + backupInstances[n] + ' -n "' + backupImgName[n] + '"'
-            print('Preforming backup of ' + backupInstances[n] + ' with reboot')
-        else:
-            ec2ImgCmd = ec2ImgCmd + ' ' + backupInstances[n] + ' -n "' + backupImgName[n] + '"' + ' --no-reboot'
-            print('Preforming backup of ' + backupInstances[n] + ' with no reboot')
-        
-        print('Creating image: ' + backupImgName[n] + '...')
-        exeEc2ImgCmd = subprocess.Popen(ec2ImgCmd,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+
+
+
+    #time.sleep(10)
+    #ec2StatusCmd = ec2StatusCmd + ' ' + backupInstances[n]
+    #exeEc2StatusCmd = subprocess.Popen(ec2StatusCmd,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+
+
