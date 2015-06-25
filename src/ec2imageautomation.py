2273bb6f7ca2c41a3f83decba4698e6e2761c211
TvHeadedRobots <pneusolematic@mac.com>
2015-06-25 15:38:43 -0400

    Log and comment clean up

--- src/ec2imageautomation.py
+++ src/ec2imageautomation.py
@@ -52,7 +52,6 @@ def get_backup_instance(backupVal):
     return ec2ImgInstanceId
 
 ### get "Name" tag name / value for each instance ID to create backup image name ###
-# iterate through ec2ImgInstanceId
 def get_backup_img_name(ec2ImgInstanceId):
     ec2ImgName = []
 
@@ -72,6 +71,7 @@ def get_backup_img_name(ec2ImgInstanceId):
     # return list of constructed images names for backup            
     return ec2ImgName
 
+### create new EC2 AMI ###
 def create_ec2_img(backupInstanceList, backupImgNameList):
     # check to make sure that backupInstanceList list is same size as backupImgName list
     
@@ -81,15 +81,17 @@ def create_ec2_img(backupInstanceList, backupImgNameList):
     ec2ImgResult = {}
     
     if len(backupInstanceList) != len(backupImgNameList):
+        amiLog.write('ERROR: List of instances to backup is not the same size as backup image name list.\t\t\t' + curdate + '\r\n')
+        amiLog.close()
         print(len(backupImgNameList)) 
-        print("ERROR: List of instances to backup is not the same size as backup image name list.\r\nSomething is wrong and I'm not going any further.")
+        print("\r\nERROR: List of instances to backup is not the same size as backup image name list.\r\nSomething is wrong and I'm not going any further.")
         print('len(backupImgNameList) = ')
         print(len(backupImgNameList))
         print('len(backupInstanceList) = ')
         print(len(backupInstanceList))
         for n in backupInstanceList:
             ec2ImgResult[backupInstanceList[n]] = "ERROR_LIST_SZ"
-        return 
+        return ec2ImgResult 
     else:
         for n in range(len(backupInstanceList)):
             ec2ImgCmd = 'c:/ec2/ec2-api-tools-1.7.4.0/bin/ec2-create-image.cmd'
@@ -115,8 +117,8 @@ def create_ec2_img(backupInstanceList, backupImgNameList):
             #check stdout result to see if an image was created
             if len(retVal) > 0 and retVal[0] == 'IMAGE':
                 amiId = retVal[1].replace('\r\n','')
-                amiLog.write('AMI created: ' + amiId + '\t' + backupImgNameList[n] + '\t' + curdate + '\r\n')
-                print('Image ' + amiId + ' created')
+                amiLog.write('AMI created:\t' + amiId + '\t' + backupImgNameList[n] + '\t' + curdate + '\r\n')
+                print('\r\nImage ' + amiId + ' created')
                 ec2ImgResult[backupInstanceList[n]] = amiId
             # check to see if there was an error returned
             if ec2ImgErrOut != '':
@@ -126,15 +128,15 @@ def create_ec2_img(backupInstanceList, backupImgNameList):
 
                 # check to see if image name already exists
                 if errRetVal[0] == 'Client.InvalidAMIName.Duplicate:':
-                    amiLog.write(backupImgNameList[n] + ' already exists\t' + curdate + '\t' + errRetVal[0] + '\r\n')
+                    amiLog.write('ERROR:\t' + backupImgNameList[n] + ' already exists\t' + errRetVal[0] + '\t' + curdate + '\r\n')
                     # add duplicate AMI names to list to rerun
                     dupeAmiNames.append(backupImgNameList[n])
-                    print('No AMI created')
+                    print('\r\nNo AMI created')
                     print(ec2ImgErrOut)
                     ec2ImgResult[backupInstanceList[n]] = "ERROR_DUPE"
                 else:
-                    amiLog.write('ERROR! stdout: ' + ec2ImgStdOut.replace('\r\n','\t') + 'errout: ' + ec2ImgStdOut.replace('\r\n', '\t'))
-                    print('Something unexpected happened...')
+                    amiLog.write('ERROR:\tstdout: ' + ec2ImgStdOut.replace('\r\n','\t') + 'errout: ' + ec2ImgStdOut.replace('\r\n', '\t'))
+                    print('\r\nSomething unexpected happened...')
                     print('stdout: ' + ec2ImgStdOut)
                     print('errout: ' + ec2ImgErrOut)
                     ec2ImgResult[backupInstanceList[n]] = ec2ImgErrOut
